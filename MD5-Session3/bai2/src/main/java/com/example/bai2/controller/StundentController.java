5f9afdd81c34a012474db272447abc30a8dcc9d3
buiduclinh <builinh120396@gmail.com>
2025-08-01 15:16:17 +0700

    Add files via upload

---
+++ MD5-Session3/bai2/src/main/java/com/example/bai2/controller/StundentController.java
@@ -0,0 +1,22 @@
+package com.example.bai2.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/")
+public class StundentController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ MD5-Session3/bai2/src/main/webapp/add.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LENOVO
+  Date: 2025/08/01
+  Time: 13:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<label for="studentId"></label>
+<input type="text" id="studentId" name="studentId">
+<label for="studentName"></label>
+<input type="text" id="studentName" name="studentName">
+<label for="studentAge"></label>
+<input type="text" id="studentAge" name="studentAge">
+<label for="studentAct"></label>
+<checkbox id="studentAct" name="studentAct">Active</checkbox>
+<checkbox id="studentAct" name="studentAct">Deactive</checkbox>
+</body>
+</html>
